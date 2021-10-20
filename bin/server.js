13b096003c0f963294b8bae37726182decf46983
Alexey Boklogov <kashira87@mail.ru>
2021-10-20 15:07:15 +0300

    create database

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,21 @@
 const app = require('../app')
+const mongoose = require('mongoose')
+const { DB_HOST } = require('../config')
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
+mongoose.connect(DB_HOST, {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  useUnifiedTopology: true
 })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+    console.log('Database connection successful')
+  })
+  .catch(err => {
+    console.log(err)
+    process.exit(1)
+  })
