8da8fab3a6804190fa20984996cd0b32a82eda16
Alexey Boklogov <kashira87@mail.ru>
2021-10-30 16:02:59 +0300

    add paginate

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,12 +1,20 @@
 const { Contact } = require('../../models')
 
 const listContacts = async (req, res) => {
-  const contacts = await Contact.find({ owner: req.user._id }).populate('owner', '_id email subscription')
+  const { page = 1, limit = 2 } = req.query
+  const skip = (page - 1) * limit
+
+  const total = await Contact.find({ owner: req.user._id })
+  const pages = Math.ceil(total.length / limit)
+
+  const contacts = await Contact.find({ owner: req.user._id }, '_id name email phone favorite owner', { skip, limit: +limit }).populate('owner', '_id email subscription')
 
   res.status(200).json({
     status: 'success',
     code: 200,
     data: {
+      total: total.length,
+      pages,
       resault: contacts
     },
   })
