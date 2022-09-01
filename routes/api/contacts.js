6cb58c9f2e99c3859aca670a6fe5923e00efea46
Алексей Боклогов <kashira87@mail.ru>
2022-09-01 09:29:54 +0300

    Фиксация изменений

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -7,15 +7,10 @@ const { validation } = require('../../middlewares')
 const { contacts: ctrl } = require('../../controllers')
 
 router.get('/', ctrl.listContacts)
-
 router.get('/:contactId', ctrl.getContactById)
-
 router.post('/', validation(joiSchema), ctrl.addContact)
-
 router.delete('/:contactId', ctrl.removeContact)
-
 router.put('/:contactId', validation(joiSchema), ctrl.updateContact)
-
 router.patch('/:contactId/favorite', validation(joiSchema), ctrl.updateStatusContact)
 
 module.exports = router
